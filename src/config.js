a7f7535d50e9737ed8948928bc708748e2d064d2
Viet <chotrongnha@gmail.com>
2023-08-04 13:21:19 +0700

    view nro- vercel

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 // config.js
 const globalConfig = {
-    apiUrl: 'http://localhost:3000/api', // Thay đổi giá trị theo yêu cầu của bạn
+  apiUrl: 'https://shopacc.onrender.com'
+
+
+    // apiUrl: 'http://localhost:3000/api', // Thay đổi giá trị theo yêu cầu của bạn
     // Định nghĩa các biến global khác nếu cần
   };
   
