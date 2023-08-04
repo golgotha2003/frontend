deee0d74aa53ae1c21e6c3e1bf57ff09ef28fe09
Viet <chotrongnha@gmail.com>
2023-08-04 17:50:41 +0700

    filter product

--- src/BodyContent/Guest/NgocRongPage/NgocRongPage.js
+++ src/BodyContent/Guest/NgocRongPage/NgocRongPage.js
@@ -4,126 +4,248 @@ import globalConfig from '../../../config';
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { Typography, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 import { Link } from 'react-router-dom';
-import CircularProgress from '@mui/material/CircularProgress'; // Import CircularProgress
-import './NgocRongPage.css';
+import CircularProgress from '@mui/material/CircularProgress';
+import Pagination from '@mui/material/Pagination';
 import { MonetizationOn } from '@mui/icons-material';
+import './NgocRongPage.css';
 
 const NgocRongPage = () => {
-  const [accounts, setAccounts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // Thêm trạng thái isLoading
+    const [accounts, setAccounts] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const ITEMS_PER_PAGE = 12;
+    const [currentPage, setCurrentPage] = useState(1);
+    const [filterServer, setFilterServer] = useState('Tất cả Server');
+    const [filterHanhTinh, setFilterHanhTinh] = useState('Tất cả Hành tinh');
+    const [filterBongTai, setFilterBongTai] = useState('Không chọn');
+    const [filterDeTu, setFilterDeTu] = useState('Không chọn');
+    const [filterGia, setFilterGia] = useState('Tất cả Giá');
 
-  useEffect(() => {
-    // Gọi API để lấy danh sách tài khoản
-    axios
-      .get(`${globalConfig.apiUrl}/ngocrong/getall`)
-      .then((response) => {
-        // Lấy danh sách tài khoản từ API
-        const fetchedAccounts = response.data.ngocrongs;
+    useEffect(() => {
+        axios
+            .get(`${globalConfig.apiUrl}/ngocrong/getall`)
+            .then((response) => {
+                const fetchedAccounts = response.data.ngocrongs;
 
-        // Lặp qua từng tài khoản và gọi API để lấy hình ảnh tương ứng
-        Promise.all(
-          fetchedAccounts.map((account) =>
-            axios
-              .get(`${globalConfig.apiUrl}/imagesngocrong/getOne/${account.id}`)
-              .then((response) => response.data.images.dataImage)
-              .catch((error) => {
-                console.error(`Error fetching image for account ${account.id}:`, error);
-                return ''; // Trả về empty string nếu không lấy được hình ảnh
-              })
-          )
-        )
-          .then((imageUrls) => {
-            // Gán imageUrl cho từng tài khoản trong danh sách tài khoản
-            const updatedAccounts = fetchedAccounts.map((account, index) => ({
-              ...account,
-              imageUrl: imageUrls[index],
-            }));
+                Promise.all(
+                    fetchedAccounts.map((account) =>
+                        axios
+                            .get(`${globalConfig.apiUrl}/imagesngocrong/getOne/${account.id}`)
+                            .then((response) => response.data.images.dataImage)
+                            .catch((error) => {
+                                console.error(`Error fetching image for account ${account.id}:`, error);
+                                return '';
+                            })
+                    )
+                )
+                    .then((imageUrls) => {
+                        const updatedAccounts = fetchedAccounts.map((account, index) => ({
+                            ...account,
+                            imageUrl: imageUrls[index],
+                        }));
 
-            // Cập nhật state với danh sách tài khoản đã có imageUrl và đặt isLoading thành false
-            setAccounts(updatedAccounts);
-            setIsLoading(false);
-          })
-          .catch((error) => {
-            console.error('Error fetching images:', error);
-            setIsLoading(false);
-          });
-      })
-      .catch((error) => {
-        console.error('Error fetching accounts:', error);
-        setIsLoading(false);
-      });
-  }, []);
-  const getPlanetName = (planetCode) => {
-    switch (planetCode) {
-      case 'Trai-dat':
-        return 'Trái đất';
-      case 'Namec':
-        return 'Namec';
-      case 'Xayda':
-        return 'Xayda';
-      default:
-        return planetCode; // Trả lại giá trị gốc nếu không có ánh xạ phù hợp
-    }
-  };
-  const getYesNoValue = (value) => {
-    return value ? "Có" : "Không";
-  };
+                        setAccounts(updatedAccounts);
+                        setIsLoading(false);
+                    })
+                    .catch((error) => {
+                        console.error('Error fetching images:', error);
+                        setIsLoading(false);
+                    });
+            })
+            .catch((error) => {
+                console.error('Error fetching accounts:', error);
+                setIsLoading(false);
+            });
+    }, []);
 
-  const CategorySection = ({ account }) => {
-    const planetName = getPlanetName(account.hanh_tinh);
+    const getPlanetName = (planetCode) => {
+        switch (planetCode) {
+            case 'Trai-dat':
+                return 'Trái đất';
+            case 'Namec':
+                return 'Namec';
+            case 'Xayda':
+                return 'Xayda';
+            default:
+                return planetCode;
+        }
+    };
+
+    const getYesNoValue = (value) => {
+        return value ? 'Có' : 'Không';
+    };
+
+    const formatAmount = (amount) => {
+        return amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    };
+
+    const CategorySection = ({ account }) => {
+        const planetName = getPlanetName(account.hanh_tinh);
+        return (
+            <Box className="categorySection">
+                <div className="product">
+                    <div className="card">
+                        {account.imageUrl && <img src={account.imageUrl} alt="Product" className="product-image" />}
+                    </div>
+
+                    <div className="attribute-content">{account.content}</div>
+
+                    <div className="attribute-container">
+                        <p className="attribute-item">Server: {account.sever}</p>
+                        <p className="attribute-item">Đệ tử: {getYesNoValue(account.de_tu)}</p>
+                        <Typography variant="body1" className="attribute-item">
+                            <MonetizationOn />{formatAmount(account.amount)} VNĐ
+                        </Typography>
+                    </div>
+                    <div className="attribute-container">
+                        <p className="attribute-item">Hành tinh: {planetName}</p>
+                        <p className="attribute-item">Bông tai: {getYesNoValue(account.bong_tai)}</p>
+                        <Link to={`/ngocrong/${account.id}`} className="viewNowButton">
+                            Xem Ngay
+                        </Link>
+                    </div>
+                </div>
+            </Box>
+        );
+    };
+
+    const handlePaginationChange = (event, value) => {
+        setCurrentPage(value);
+    };
+
+    const applyFilters = (account) => {
+        if (filterServer != 'Tất cả Server' && account.sever !== filterServer) {
+            return false;
+        }
+        if (filterHanhTinh != 'Tất cả Hành tinh' && account.hanh_tinh !== filterHanhTinh) {
+            return false;
+        }
+        if (filterBongTai !== 'Không chọn' && account.bong_tai !== filterBongTai) {
+            return false;
+        }
+        if (filterDeTu !== 'Không chọn' && account.de_tu !== filterDeTu) {
+            return false;
+        }
+        if (filterGia && filterGia !== '') {
+            const amount = parseInt(account.amount);
+            switch (filterGia) {
+                case 50000:
+                    if (amount >= 50000) {
+                        return false;
+                    }
+                    break;
+                case 200000:
+                    if (amount < 50000 || amount >= 200000) {
+                        return false;
+                    }
+                    break;
+                case 500000:
+                    if (amount < 200000 || amount >= 500000) {
+                        return false;
+                    }
+                    break;
+                case 1000000:
+                    if (amount < 500000 || amount >= 1000000) {
+                        return false;
+                    }
+                    break;
+                case 2000000:
+                    if (amount < 1000000) {
+                        return false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return true;
+    };
+
+    const filteredAccounts = accounts.filter(applyFilters);
+    const totalPages = Math.ceil(filteredAccounts.length / ITEMS_PER_PAGE);
+    const displayedAccounts = filteredAccounts.slice((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE);
+
     return (
-      <Box className="categorySection">
-        <div className="product">
-          <div className="card">
-            {account.imageUrl && <img src={account.imageUrl} alt="Product" className="product-image" />}
-          </div>
-          
-          <div className="attribute-content">
-             {account.content}
-          </div>
-          
-          <div className="attribute-container">
-            <p className="attribute-item">Server: {account.sever}</p>
-            <p className="attribute-item">Đệ tử: {getYesNoValue(account.de_tu)}</p>
-            <Typography variant="body1" className="attribute-item">
-          <MonetizationOn />{account.amount} VNĐ
-        </Typography>
-            
-          </div>
-          <div className="attribute-container">
-            <p className="attribute-item">Hành tinh: {planetName}</p>
-            <p className="attribute-item">Bông tai: {getYesNoValue(account.bong_tai)}</p>
-            <Link to={`/ngocrong/${account.id}`} className="viewNowButton">
-              Xem Ngay
-            </Link>
-          </div>
-        </div>
-      </Box>
-    );
-  };
+        <Container component="main" maxWidth="md">
+            {isLoading ? (
+                <div className="loadingContainer">
+                    <CircularProgress />
+                </div>
+            ) : (
+                <>
+                    <h1 variant="h1">Các tài khoản NgocRong đang bán</h1>
+                    <Box className="filter-container">
+                        <FormControl variant="outlined" className="filter-control">
+                            <InputLabel>Server</InputLabel>
+                            <Select value={filterServer} onChange={(e) => setFilterServer(e.target.value)} label='Server'>
+                                <MenuItem value="Tất cả Server">Tất cả Server</MenuItem>
 
-  return (
-    <Container component="main" maxWidth="md">
-      {isLoading ? (
-        <div className="loadingContainer">
-          <CircularProgress />
-        </div>
-      ) : (
-        <>
-          <h1 variant="h1">Các tài khoản NgocRong đang bán</h1>
-          <Row>
-            {accounts.map((account) => (
-              <Col key={account.id} xs={12} sm={6} md={4} lg={3}>
-                <CategorySection account={account} />
-              </Col>
-            ))}
-          </Row>
-        </>
-      )}
-    </Container>
-  );
+                                <MenuItem value="1">1</MenuItem>
+                                <MenuItem value="2">2</MenuItem>
+                                <MenuItem value="3">3</MenuItem>
+                                <MenuItem value="4">4</MenuItem>
+                                <MenuItem value="5">5</MenuItem>
+                                <MenuItem value="6">6</MenuItem>
+                                <MenuItem value="7">7</MenuItem>
+                                <MenuItem value="8">8</MenuItem>
+                                <MenuItem value="9">9</MenuItem>
+                                <MenuItem value="10">10</MenuItem>
+                                <MenuItem value="11">11</MenuItem>
+
+                            </Select>
+                        </FormControl>
+                        <FormControl variant="outlined" className="filter-control">
+                            <InputLabel>Hành tinh</InputLabel>
+                            <Select value={filterHanhTinh} onChange={(e) => setFilterHanhTinh(e.target.value)}>
+                                <MenuItem value="Tất cả Hành tinh">Tất cả Hành tinh</MenuItem>
+                                <MenuItem value="Trai-dat">Trái Đất</MenuItem>
+                                <MenuItem value="Namec">Namec</MenuItem>
+                                <MenuItem value="Xayda">Xayda</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl variant="outlined" className="filter-control">
+                            <InputLabel>Bông Tai</InputLabel>
+                            <Select value={filterBongTai} onChange={(e) => setFilterBongTai(e.target.value)}>
+                                <MenuItem value="Không chọn">Không chọn</MenuItem>
+                                <MenuItem value={false}>Không</MenuItem>
+                                <MenuItem value={true}>Có</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl variant="outlined" className="filter-control">
+                            <InputLabel>Đệ tử</InputLabel>
+                            <Select value={filterDeTu} onChange={(e) => setFilterDeTu(e.target.value)}>
+                                <MenuItem value="Không chọn">Không chọn</MenuItem>
+                                <MenuItem value={false}>Không</MenuItem>
+                                <MenuItem value={true}>Có</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl variant="outlined" className="filter-control">
+                            <InputLabel>Giá</InputLabel>
+                            <Select value={filterGia} onChange={(e) => setFilterGia(e.target.value)}>
+                                <MenuItem value="Tất cả Giá">Tất cả Giá</MenuItem>
+                                <MenuItem value={50000}>Dưới 50k</MenuItem>
+                                <MenuItem value={200000}>Từ 50k đến 200k</MenuItem>
+                                <MenuItem value={500000}>Từ 200k đến 500k</MenuItem>
+                                <MenuItem value={1000000}>Từ 500k đến 1 triệu</MenuItem>
+                                <MenuItem value={2000000}>Trên 1 triệu</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                    <Row>
+                        {displayedAccounts.map((account) => (
+                            <Col key={account.id} xs={12} sm={6} md={4} lg={3}>
+                                <CategorySection account={account} />
+                            </Col>
+                        ))}
+                    </Row>
+                    <div className="pagination">
+                        <Pagination count={totalPages} page={currentPage} onChange={handlePaginationChange} color="primary" />
+                    </div>
+                </>
+            )}
+        </Container>
+    );
 };
 
 export default NgocRongPage;
